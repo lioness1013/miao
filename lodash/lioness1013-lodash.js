bccfd36d34374bfaff4ce09bca5736a07d08d67b
duoduo <duoduo@ChendeMacBook-Pro.local>
2019-01-10 11:31:26 +0800

    update

--- lodash/lioness1013-lodash.js
+++ lodash/lioness1013-lodash.js
@@ -1,14 +1,26 @@
-var lioness1013 = function(){
+var lioness1013 = function () {
 
   function chunk(ary, size = 1) {
 
   }
-  function compact(ary) {
+  function compact(array) {
+    var index = -1,
+      length = array == null ? 0 : array.length,
+      resIndex = 0,
+      result = [];
 
+    while (++index < length) {
+      var value = array[index];
+      if (value) {
+        result[resIndex++] = value;
+      }
+    }
+    return result;
   }
 
-  return {
-    chunk: chunk,
-    compact: compact,
-  }
+
+return {
+  chunk: chunk,
+  compact: compact,
+}
 }()
